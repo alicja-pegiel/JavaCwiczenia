5ae4a3d51df14b37d8383125836d166b875effaf
Alicja Gorgoń <alicja.pegiel@zskocjan.pl>
2025-09-10 10:34:32 +0200

    Klasa Hello

---
+++ PodstawyJavy/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        // Zad.1 Wypisz swoje imię i klasę
+        System.out.println("Alicja 5cI");
+        // Komentowanie wielu linijek - ctrl + /
+        /*
+        To jest komentarz wieloliniowy.
+        Mogę pisać po enterze i dalej jestem
+        w komentarzu.
+         */
+    }
+}
