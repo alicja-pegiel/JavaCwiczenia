b746f8259b5ed7d388cbf3d74b19e4835d1c32db
Alicja Gorgoń <alicja.pegiel@zskocjan.pl>
2025-09-10 11:30:31 +0200

    Klasa Hello

--- PodstawyJavy/src/Hello.java
+++ PodstawyJavy/src/Hello.java
@@ -9,5 +9,25 @@ public class Hello {
         Mogę pisać po enterze i dalej jestem
         w komentarzu.
          */
+
+        int a = 5, b = 2;
+        System.out.println("Wynik dodawania: " + (a+b));
+        float wynik = 0;
+        wynik = (float)a/b;
+        System.out.println(wynik);
+        /*
+        \t - tab
+        \n - nowa linia
+        \r - powrót karetki
+        \" - cudzysłów
+        \' - apostrof
+        \\ - backslash
+         */
+        System.out.println("Tu jest coś\t\t\t\t\t A tu co innego");
+        System.out.println("Zdanie\n");
+        //System.out.println();
+        System.out.println("Drugie zdanie");
+        System.out.println("Do robienia znaków specjalnych używamy znaku \\");
+        System.out.println("Do wpisywania Stringów używamu znaku \"");
     }
 }
--- PodstawyJavy/src/Main.java
+++ PodstawyJavy/src/Main.java
@@ -2,6 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        boolean zm = true;
+        if (!zm) {
+            System.out.println("Zmienna to prawda");
+        } else {
+            System.out.println("Zmienna to fałsz");
+        }
+        String napis = "Napis to jest.";
+        napis = napis + " A to jest kolejny napis";
+        System.out.println(napis);
+        System.out.println('a' + 0);
 
+        // Zad.2 Stwórz program, który będzie obliczał pole koła o r = 4
+        System.out.println("Pole koła to: " + (4*4*3.14));
+        System.out.println("Pole koła to: " + (4*4*Math.PI));
     }
 }
